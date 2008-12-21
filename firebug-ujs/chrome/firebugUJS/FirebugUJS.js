41d92f92660067b5eedf8a439826fd30cd8534bb
remi <remi@remitaylor.com>
2008-12-21 15:15:36 -0700

    added gitignore & got my panel showing up under HTML, like i want it ... now i need a reference to currently selected DOM element

--- firebug-ujs/chrome/firebugUJS/FirebugUJS.js
+++ firebug-ujs/chrome/firebugUJS/FirebugUJS.js
@@ -1,35 +1,48 @@
-FBL.ns(function() { with (FBL) { 
-Firebug.FirebugUJS = extend(Firebug.Module, { 
-  shutdown: function() {
-    if(Firebug.getPref('defaultPanelName')=='FirebugUJS') {
-      /* Optional */
-      Firebug.setPref('defaultPanelName','console');
-    }
-  },
-  showPanel: function(browser, panel) { 
-    var isFirebugUJS = panel && panel.name == "FirebugUJS"; 
-    var FirebugUJSButtons = browser.chrome.$("fbFirebugUJSButtons"); 
-    collapse(FirebugUJSButtons, !isFirebugUJS); 
-  }, 
-  button1: function() { 
-    FirebugContext.getPanel("FirebugUJS").printLine('Clicked Button 1'); 
-  }, 
-  button2: function() { 
-    FirebugContext.getPanel("FirebugUJS").printLine('Clicked Button 2'); 
-  } 
-}); 
-function FirebugUJSPanel() {} 
-FirebugUJSPanel.prototype = extend(Firebug.Panel, { 
-  name: "FirebugUJS", 
-  title: "Test Panel", 
-  searchable: false, 
-  editable: false,
-  printLine: function(message) {
-    var elt = this.document.createElement("p");
-    elt.innerHTML = message;
-    this.panelNode.appendChild(elt);
-  }
-}); 
-Firebug.registerModule(Firebug.FirebugUJS); 
-Firebug.registerPanel(FirebugUJSPanel); 
+FBL.ns( function() { with (FBL) { 
+
+	Firebug.FirebugUJS = extend(Firebug.Module, { 
+
+	  shutdown: function() {
+	    if(Firebug.getPref('defaultPanelName')=='FirebugUJS') {
+	      /* Optional */
+	      Firebug.setPref('defaultPanelName','console');
+	    }
+	  },
+
+	  showPanel: function(browser, panel) { 
+	    var isFirebugUJS = panel && panel.name == "FirebugUJS"; 
+	    var FirebugUJSButtons = browser.chrome.$("fbFirebugUJSButtons"); 
+	    collapse(FirebugUJSButtons, !isFirebugUJS); 
+	  }, 
+
+	  button1: function() { 
+	    FirebugContext.getPanel("FirebugUJS").printLine('well hello there!'); 
+	    // FirebugContext.getPanel
+	    // parentPanel [string, int], [string, 4]: html ...
+	    FirebugContext.getPanel("FirebugUJS").printLine( FirebugContext.getPanel('html') ); 
+	  }, 
+
+	  button2: function() { 
+	    FirebugContext.getPanel("FirebugUJS").printLine('Clicked Button 2'); 
+	  } 
+	}); 
+
+	function FirebugUJSPanel() {} 
+
+	FirebugUJSPanel.prototype = extend(Firebug.Panel, { 
+	  name: "FirebugUJS", 
+	  title: "Hello From Test Panel", 
+	  parentPanel: "html",
+	  searchable: false, 
+	  editable: false,
+	  printLine: function(message) {
+	    var elt = this.document.createElement("p");
+	    elt.innerHTML = message;
+	    this.panelNode.appendChild(elt);
+	  }
+	}); 
+
+	Firebug.registerModule(Firebug.FirebugUJS); 
+	Firebug.registerPanel(FirebugUJSPanel); 
+
 }});
