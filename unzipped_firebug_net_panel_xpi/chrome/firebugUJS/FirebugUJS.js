82a56587f798fcdd639bc7b93bfa040028bede03
remi <remi@remitaylor.com>
2008-12-21 13:09:38 -0700

    probably explodes ... just starting to tweak the net panel extension to get my own one working ... trying to learn as i go ...

--- unzipped_firebug_net_panel_xpi/chrome/firebugUJS/FirebugUJS.js
+++ unzipped_firebug_net_panel_xpi/chrome/firebugUJS/FirebugUJS.js
@@ -1,25 +1,3 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * 
- * The contents of this file are subject to the Mozilla Public License
- * Version 1.1 (the "License"); you may not use this file except in
- * compliance with the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- * 
- * Software distributed under the License is distributed on an "AS IS"
- * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
- * License for the specific language governing rights and limitations
- * under the License.
- * 
- * The Original Code is Firebug Net Panel History Overlay.
- * 
- * The Initial Developer of the Original Code is Mihailo Lalevic.
- * Portions created by Initial Developer are Copyright (C) 2008 the
- * Initial Developer. All Rights Reserved.
- * 
- * Contributor(s): 
- * 
- * ***** END LICENSE BLOCK ***** */
-
 //gets panel prototype from the panel type list
 function getPanelPrototype(aName) {
     for (var i = 0; i < Firebug.panelTypes.length; i++) {
@@ -83,7 +61,7 @@ function fakeNetProgress(netProgress){
   }
 }
 
-Firebug.NetPanelHistoryOverlayModel = extend(Firebug.Module, 
+Firebug.FirebugUJS = extend(Firebug.Module, 
 { 
     historyData: [], //keeps the history data
 
@@ -436,8 +414,7 @@ Firebug.NetPanelHistoryOverlayModel = extend(Firebug.Module,
     }
 }); 
 
-Firebug.registerModule(Firebug.NetPanelHistoryOverlayModel);
+// REGISTER WITH FIREBUG!
+Firebug.registerModule(Firebug.FirebugUJS);
 
 }});
-
-//----------------------------------------------------------------------------- 
